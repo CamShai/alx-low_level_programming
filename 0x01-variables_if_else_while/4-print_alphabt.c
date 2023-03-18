eb0aa8492cd593d759ca526b752e19ee08bbd7e7
CamShai <eugenejohn10@gmail.com>
2023-03-18 16:42:50 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- is the main function
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z' && i != 'q' && i != 'e'; i++)
+		putchar (i);
+	putchar ('\n');
+	return (0);
+}
