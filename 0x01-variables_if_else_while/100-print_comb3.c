320fee3732a506a5248b9374a633baef610ff2df
CamShai <eugenejohn10@gmail.com>
2023-03-19 15:13:17 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main-is the main function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(',');
+				putchar(' ');
+				putchar(j);
+			}
+		}
+	}
+	return (0);
+}
