664557de2b49dcc0ad5d5b206d56cc309c672617
CamShai <eugenejohn10@gmail.com>
2023-03-19 08:40:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- is main function
+ *
+ * Return: 0 when success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i <= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
+
