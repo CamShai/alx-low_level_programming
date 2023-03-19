9a953d4386ac35e1ff003042c2fb56225ce32156
CamShai <eugenejohn10@gmail.com>
2023-03-19 15:21:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,13 @@ int main(void)
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
-			putchar(i);
 		{
 			if (i < j)
 			{
+				putchar(i);
+				putchar(j);
 				putchar(',');
 				putchar(' ');
-				putchar(j);
 			}
 		}
 	}
