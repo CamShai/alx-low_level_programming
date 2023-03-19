8eb2111a549b5db2acfa1220370a508613051d34
CamShai <eugenejohn10@gmail.com>
2023-03-19 08:21:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- is the main function
+ *
+ * Return: 0 if successfull
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	return (0);
+}
