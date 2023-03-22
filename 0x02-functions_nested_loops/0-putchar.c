77fa0110159981d17371d97776b2b91ee8489ef6
CamShai <eugenejohn10@gmail.com>
2023-03-22 12:54:17 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main- is the main function
- * Return: returns 0
- */
+* main - print _putchar
+*
+* Return: 0
+*/
+
+
 int main(void)
 {
-	putchar('_putchar\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
+
