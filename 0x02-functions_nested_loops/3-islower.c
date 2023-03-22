27a354e5c0a0246746d8a1329bc164aac2104143
CamShai <eugenejohn10@gmail.com>
2023-03-22 13:48:35 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,8 @@
  * main- find out if c is lowercase
  *Return:0 if unsuccessful and 1 if success
  *int c- find if c is lowercase
+ *_islower- is function we use
+ *@c- character we check to see if lowercase
  */
 int _islower(int c)
 {
