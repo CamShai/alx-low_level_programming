2b89cc1da4cbd2abcb252ed94fbdd81b50daddae
CamShai <eugenejohn10@gmail.com>
2023-04-03 12:10:37 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strspn - count num of chars
+ * @s: final destination of bytes
+ * @accept: bytes come from here
+ * Return: num of bytes in s from accept
+ * Description: bytes from accept present in S are counted and returned
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
