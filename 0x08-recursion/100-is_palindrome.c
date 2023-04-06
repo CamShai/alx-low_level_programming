3b2d268f56e120f0d175754477b3643373de5419
CamShai <eugenejohn10@gmail.com>
2023-04-06 15:52:36 -0700

    palindrome for gods sake

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome
+ *
+ * @s: base case
+ * Return: 1 if string is a palindrome, otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	for (int i = 0; i < len / 2; i++)
+{
+	if (s[i] != s[len - i - 1])
+	{
+		return (0);
+	}
+}
+	return (1);
+}
