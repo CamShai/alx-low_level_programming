603a1365e2282b21c07c474172dfbed28cdcee79
CamShai <eugenejohn10@gmail.com>
2023-04-12 14:18:53 -0700

    add all arguments

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,6 @@ char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *_strdup(char *str);
+char *argstostr(int ac, char **av);
 
 #endif
