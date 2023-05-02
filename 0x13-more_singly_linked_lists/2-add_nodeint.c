5caf42aacd33afbe7aa8e6ac92c9d6e39c411a3e
Eugene Oyieyo <eugenejohn10@gmail.com>
2023-05-03 01:38:23 +0300

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * add_nodeint - Add new node at the beginning of a listint_t list.
  * @head: A pointer to a pointer to the head of the listint_t list.
  * @n: The integer to store in the new node.
  *
