ec1fcacdb558186ae14e069526869040b4188ab2
CamShai <eugenejohn10@gmail.com>
2023-08-29 16:18:37 +0300

    correction on strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  *_strstr - finds first occurence of substring needle in string haystack
  *@needle: substring
  *@haystack: string
- *Return: pointer to the beginning of the located substring or null if not found
+ *Return: to beginning of located substring or null if not found
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -12,15 +12,14 @@ char *_strstr(char *haystack, char *needle)
 	int j;
 
 	i = 0;
-	while ( needle[i] != '\0')
+	while (needle[i] != '\0')
 	{
 		j = 0;
-		while ( haystack[j] != '\0')
+		while (haystack[j] != '\0')
 		{
 			if (haystack[j] == needle[i])
 			{
 				return (haystack + j);
-				break;
 			}
 			j++;
 		}
