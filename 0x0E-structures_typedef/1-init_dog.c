714e72b3de87c75d3bd47f78d9cefbd84ee44af1
CamShai <eugenejohn10@gmail.com>
2023-09-24 00:14:20 +0300

    structdef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "dog.h"
-/**
- * @d - pointer dog
- * @name:name
- * @age:age
- * @owner:owner
- * Return: nothing
- */
+#include <stddef.h>
 
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: A pointer to the dog structure to initialize
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,15 @@
-5-free_dog.c
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees dogs
+ * @d: pointer to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
